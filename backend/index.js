d1e6f3a307b064e91c8c04d616f5931c426aeed7
techux <connect.world12345@gmail.com>
2025-03-25 02:59:59 +0530

    Added login and register feature

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const dbConnect = require('./utils/dbConnect');
 
+const authRoute = require('./routes/auth.route');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,11 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    return res.send("Hello World!");
+    return res.status(200).json({
+        status: "ok",
+        message: "Server Working Properly, Welcome to the API, by Devesh Singh",
+    })
 })
 
+
+app.use("/auth", authRoute);
+
+
 app.all("*", (req, res) => {
-    return res.status(404).send("Not Found");
+    return res.status(404).json({
+        status: "error",
+        message: "Resource Not Found on Server",
+    })
 })
 
 
