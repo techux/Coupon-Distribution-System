b2b78acd929ab871bab7d59f109f8b652daf08d9
techux <connect.world12345@gmail.com>
2025-03-26 16:59:48 +0530

    fixed cors issue

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const couponRoute = require('./routes/coupon.route');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
